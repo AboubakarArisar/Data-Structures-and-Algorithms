51ad511a2610f8ce563ac5afe2ecfd7250eb8c7c
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-01 23:39:52 +0500

    Bubble sorting in ascending order in java DSA

---
+++ Sorting/BubbleSorting.java
@@ -0,0 +1,29 @@
+
+/*
+Bubble Sorting Asscending order
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    int array[] = {5,7,8,4,2};
+    for(int i = 0 ; i < array.length -1 ; i++){
+      for(int j = 0; j < array.length - i - 1; j++){
+        if(array[j] > array[j+1]){
+          int temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+      }
+      
+    }
+    for(int arr : array){
+          System.out.print(arr + " ");
+      }
+  }
+}
+
+/*
+
+Time complexity in Worst case O(n);
+
+*/
