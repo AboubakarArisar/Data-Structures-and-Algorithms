85f9f041944eff10afdbba42d732089b3aaa6cd3
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2023-12-02 00:18:38 +0500

    Insertion Sorting Descending order DSA java

---
+++ Sorting/Insertion-Sorting.java
@@ -0,0 +1,28 @@
+
+/*
+Insertion Sorting Descending order
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    int array[] = {5,7,8,4,2};
+    for(int i = 1 ; i < array.length ; i++){
+      int current = array[i];
+      int key = i - 1;
+      while(key >=0 && current > array[key]) {
+        
+        array[key+1] = array[key];
+        key--;
+      }
+      array[key+1] = current;
+      
+    }
+    for(int arr : array){
+          System.out.print(arr + " ");
+      }
+  }
+}
+
+/*
+Time complexity worst case O(n^2)
+*/
